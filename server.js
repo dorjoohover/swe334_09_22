3b55744a1d7f76346955d6bce316784e1dc452bf
dorjoo <dorjoohover@gmail.com>
2025-10-06 16:21:43 +0800

    a

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require("express");
+const bodyParser = require('body-parser')
 const { connectDB } = require("./db");
 
 const PORT = 5000;
+
+const errorHandler = require("./middleware/error");
+const logger = require("./middleware/logger");
+
 const categoriesRoutes = require("./routes/categories");
-const authRoutes = require("./routes/auth"); // Шинэ auth route
+const authRoutes = require("./routes/auth"); 
+const appRoutes = require("./routes/app"); 
 
 const app = express();
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(logger);
+app.use(errorHandler);
+app.use('/api', appRoutes)
 app.use("/api/categories", categoriesRoutes);
 app.use("/api/auth", authRoutes);
 
